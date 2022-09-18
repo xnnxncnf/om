4ed6618a802c38836aa9c47b423b8aec74827b18
xnnxncnf <112460221+xnnxncnf@users.noreply.github.com>
2022-09-18 08:30:54 +0800

    Update config.php

--- .data/config.php
+++ .data/config.php
@@ -1,12 +1,4 @@
 <?php $configs = '
-{"disktag": "tgding",
-    "passfile": "p",
-    "sitename": "\u2230666\u2230\u2230",
-    "timezone": "8",
-    "tgding": {
-        "Driver": "Onedrive",
-        "diskname": "YmdlaW5nYmFjaw",
-        "refresh_token": "0.AT4AZzKR4gOB60idw6oZctfS9ij5TnNM11VFjRvZQvoKGkE-AGM.AgABAAEAAAD--DLA3VO7QrddgJg7WevrAgDs_wQA9P9SacKziLtGJhPmpmJn2eTxQzHNhXF8UfRddjUfz-v4NI4EpXpXgeY3KS34ThNS4PeXZ3dUpUJfGF466rP4owcjiiHpgdcc43q2bs2FzWEzPRbZ_2BnvnvY9m5WVo6mfu493qlzzwR4tWHnHdituGAlYfrSrrVnsZ6_q4ygfrgQPnKnE1QlwOVXj_6eFZO81YamWVIaO9YTXQ_gJx8OS4gOmvfqkPj19yOqPyM6xUtTBzxHPZz1mT1foaHl_won52-D-F3-gmnpRMcKq-tNKvZXu51akn2zP30DfeNax4rqpnQXr1VJ4IPKt_qITYwbnV9To-A5gX8J6Y5RUu1rfzPUmthzhabTV_Q8nyO7o-MTMR6UdL6EmyRoGL55qszN6n2nJh8d7GMfIQG4TQReyLiXBaneyfqZ1ASinAmL3TMRQau5qnwgUdRBw3J6GlEia2r4aOLwQ7U5-ke5DMoKruHFcz4KMgPWrt3QGnrxEyE9Ow5tiI-XGN2FQfj9lrMIuj2U_vtI4QVkjGaKSbysEsTDcV5gUvOmDAZ3q7VSqbcXKbfa5ecRovRgQOMC5BrUIQBOR383jv7eMQs4u6mHSRdJFJGx8WaSJ5BeVuup6RpEY1IMKuKeVGrzJ10vKQhoLrGqjEJrjlwqteVqxtFUV87XuZRNQkutBY1PrRJh7SJVD5DsYjUD-X7WMtjjs4sU6YfeufW3VwK29wFsQN6wy729FnKS5kRx65DB0myblQ",
-        "token_expires": 1663743718
-    }
-};
+{
+},
+;
